667067045e5291feda10a4b0f9bf70e77274098a
sjbdcs <91319176+sjbdcs@users.noreply.github.com>
2022-10-10 15:33:56 +0300

    Update Logo.vue

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -2,12 +2,12 @@
   <g-link to="/" class="logo" title="Back to home">
     <transition name="theme">
       <div v-if="color == 'dark'">
-        <g-image src="~/assets/img/logo.png" alt="logo" />
+        <g-image src="/static/logo.png" alt="logo" />
       </div>
     </transition>
     <transition name="theme">
       <div v-if="color == 'bright'">
-        <g-image src="~/assets/img/logo.png" alt="logo" />
+        <g-image src="/static/logo.png" alt="logo" />
       </div>
     </transition>
   </g-link>
