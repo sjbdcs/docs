bf0b5de7419173e22b749b9bf6b8ce31b97f38cf
sjbdcs <91319176+sjbdcs@users.noreply.github.com>
2022-10-10 15:10:13 +0300

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,9 +4,9 @@
       <h1>{{ $static.metadata.siteName }} - {{ this.description }}</h1>
       <nav>
         <!-- To use other icons here, you need to import them in the Shortcut component -->
-        <Shortcut link="/getting-started" text="Introduction" icon="play-icon" />
-        <Shortcut link="/theme-configuration" text="Configuration" icon="sliders-icon" />
-        <Shortcut link="/theme-configuration#changing-colors" text="Change colors" icon="eye-icon" />
+        <Shortcut link="/index" text="Read Wiki" icon="play-icon" />
+        <Shortcut link="https://sjbdcs.org" text="Home" icon="sliders-icon" />
+        <Shortcut link="mailto:webmaster@sjbdcs.org" text="Contact Support" icon="eye-icon" />
       </nav>
       <GitLink class="git" size="large" />
     </div>
@@ -32,14 +32,14 @@ export default {
   },
   data() {
     return {
-      description: 'The ultimate static generated documentation theme for the JAM-stack'
+      description: 'St John Wiki'
     }
   },
   metaInfo() {
     return {
       title: this.description,
       meta: [
-        { key: 'description', name: 'description', content: 'A theme for static site documentation based on Gridsome, ready to deploy to Netlify in one click.' }
+        { key: 'description', name: 'description', content: 'St John Baptist De La Salle Catholic School Wiki.' }
       ]
     }
   }
